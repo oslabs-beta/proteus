0b11809f52b18de9e54d8b25b0643659a2ffbefd
Mark Bryan <marklawbryan@gmail.com>
2023-02-04 12:11:36 -0800

    added styling and components

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,6 +3,17 @@ import '../Styles/home.css';
 
 export const Home = () => {
   return (
-    <div className='home-container'>Home</div>
+    <div className='home-container'>
+      <div className="home-title">
+        <div><b>cluster:</b> eks-cluster-01</div>
+        <div><b>namespace:</b> default</div>
+      </div>
+      <div className="home-schedule"></div>
+      <div className="home-job-list">
+        <div className="home-job"></div>
+        <div className="home-job"></div>
+        <div className="home-job"></div>
+      </div>
+    </div>
   )
 }
