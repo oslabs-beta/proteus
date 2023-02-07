c3ad0ba4447c0f7524c74c8fb9e136ca497ec0e9
EddyKaggia <kagzcodes@gmail.com>
2023-02-06 16:25:22 -0800

    updating home page

--- src/Components/Fetch.tsx
+++ src/Components/Fetch.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
 
-const jobsArray = [];
-const cronJobArray = [];
+//Array containing cronjobs
+const cronjobs:Array<string> = [];
+//jobs
+const jobs:Array<string> = [];
+
 fetch('http://localhost:9090/api/v1/label/job_name/values', {
   method: 'GET',
   // body: JSON.stringify(),
@@ -10,17 +13,28 @@ fetch('http://localhost:9090/api/v1/label/job_name/values', {
   }
 }).then(data => data.json())
 .then(response => {
+  const allJobsArray = response.data;
+  // console.log(allJobsArray);
 
-  console.log(response.data)
-
-  
-  fetch(`http://localhost:9090/api/v1/query?query={job_name='whalesay'}`, {
+  for(let i = 0; i < allJobsArray.length; i++) {
+    fetch(`http://localhost:9090/api/v1/query?query={job_name="${allJobsArray[i]}"}`, {
     method: 'GET',
     // body: JSON.stringify(),
     headers: {
         'Content-Type': 'application/json'
     }
   }).then(data => data.json())
-  .then(response => console.log(response));
-
+  .then(response => {
+    if(response.data.result.length !== 0){
+      // console.log(allJobsArray[i])
+      jobs.push(response.data.result);
+      console.log(jobs[0])
+    } else {
+      cronjobs.push(allJobsArray[i]);
+      // console.log('this is cronjobs', cronjobs)
+    }
   });
+  }
+  });
+
+  //cronjobs
